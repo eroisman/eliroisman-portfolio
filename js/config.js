bffe2668b32cd49eb16455d2eb381dd2a206fc0b
Eli <eliroisman1818@gmail.com>
2024-03-30 16:36:35 +0100

    MAJ

--- js/config.js
+++
@@ -1,3 +0,0 @@
-var apiKeyNewsAPI = "847feb52394b4d4a8c1abbf764b33ef2";
-
-var apiKeyGNews = "5fb7ac25da92876ef1a234673e7eca0a";
--- js/scripts.js
+++ js/scripts.js
@@ -58,7 +58,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
 
 
-
+/*
 // Define the API endpoint URL with the API key (NewsAPI.com - Pour localhost)
 var url = 'https://newsapi.org/v2/everything?' +
 'q=rançongiciel&' + 
@@ -67,12 +67,12 @@ var url = 'https://newsapi.org/v2/everything?' +
 'sortBy=publishedAt&' +
 'pageSize=9&' +
 'apiKey=847feb52394b4d4a8c1abbf764b33ef2';
-
-/*
-// Define the API endpoint URL with the API key (GNews.io)
-apikey = '5fb7ac25da92876ef1a234673e7eca0a';
-url = 'https://gnews.io/api/v4/search?q=rançongiciel&lang=fr&country=fr&max=9&apikey=' + apikey;
 */
+
+// Define the API endpoint URL with the API key (GNews.io)
+var apikey = '5fb7ac25da92876ef1a234673e7eca0a';
+var url = 'https://gnews.io/api/v4/search?q=rançongiciel&lang=fr&country=fr&max=9&apikey=' + apikey;
+
 
 // Make a fetch request to the API
 fetch(url)
